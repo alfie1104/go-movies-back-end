b3bc86d995928f6890a57c3837324238a0049cce
peter_alfie <alfie1104@gmail.com>
2023-01-09 08:34:18 +0900

    Returning a list of movies as JSON

--- cmd/api/handler.go
+++ cmd/api/handler.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"backend/internal/models"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request){
@@ -19,6 +21,50 @@ func (app *application) Home(w http.ResponseWriter, r *http.Request){
 
 	out, err := json.Marshal(payload)
 
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type","application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(out)
+}
+
+func (app *application) AllMovies(w http.ResponseWriter, r *http.Request){
+	var movies []models.Movie
+
+	rd, _ := time.Parse("2006-01-02", "1986-03-07")
+
+	highlander := models.Movie{
+		ID:  1,
+		Title : "Highlander",
+		ReleaseDate: rd,
+		MPAARating: "R",
+		RunTime: 116,
+		Description: "A very nice movie",
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	movies = append(movies, highlander)
+
+	rd, _ = time.Parse("2006-01-02", "1981-06-12")
+
+	rota := models.Movie{
+		ID:  2,
+		Title : "Raiders of the Lost Ark",
+		ReleaseDate: rd,
+		MPAARating: "PG-13",
+		RunTime: 115,
+		Description: "Another very nice movie",
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	movies = append(movies, rota)
+
+	out, err := json.Marshal(movies)
 
 	if err != nil {
 		fmt.Println(err)
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -15,5 +15,7 @@ func (app *application) routes() http.Handler {
 
 	mux.Get("/", app.Home)
 
+	mux.Get("/movies", app.AllMovies)
+
 	return mux
 }

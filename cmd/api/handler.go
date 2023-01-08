1e2254e2f7e3b980620ab36d798f940a5adcc8fc
peter_alfie <alfie1104@gmail.com>
2023-01-09 08:24:28 +0900

    Returning JSON from API

--- cmd/api/handler.go
+++ cmd/api/handler.go
@@ -1,10 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "Hello world from %s", app.Domain)
+	var payload = struct {
+		Status string `json:"status"`
+		Message string `json:"message"`
+		Version string `json:"version"`
+	}{
+		Status : "active",
+		Message : "Go Movies up and running",
+		Version: "1.0.0",
+	}
+
+	out, err := json.Marshal(payload)
+
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type","application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(out)
 }

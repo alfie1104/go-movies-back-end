e348658e5bcc0d224727b7fb6bd753f3e61ef0ea
peter_alfie <alfie1104@gmail.com>
2023-01-10 08:06:54 +0900

    Add file for setting up database using docker

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func (app *application) enableCORS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-		w.Header().Set("Access-Control-Allow-Origin","*")
+		w.Header().Set("Access-Control-Allow-Origin","http://localhost:3000")
 
 		if r.Method == "OPTIONS" {
 			w.Header().Set("Access-Control-Allow-Credentials","true")

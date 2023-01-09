631ae21c7b091cba972ec7c0319e59c2fcbcd784
peter_alfie <alfie1104@gmail.com>
2023-01-10 07:59:14 +0900

    minor modification

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func (app *application) enableCORS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-		w.Header().Set("Access-Control-Allow-Origin","http://*")
+		w.Header().Set("Access-Control-Allow-Origin","*")
 
 		if r.Method == "OPTIONS" {
 			w.Header().Set("Access-Control-Allow-Credentials","true")

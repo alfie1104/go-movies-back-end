e7a7d582ddd978ae96b23fca508d168d429c7493
peter_alfie <alfie1104@gmail.com>
2023-01-11 08:35:27 +0900

    Improving all movies handler to use real database

--- cmd/api/handler.go
+++ cmd/api/handler.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"backend/internal/models"
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"time"
 )
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request){
@@ -32,37 +30,11 @@ func (app *application) Home(w http.ResponseWriter, r *http.Request){
 }
 
 func (app *application) AllMovies(w http.ResponseWriter, r *http.Request){
-	var movies []models.Movie
-
-	rd, _ := time.Parse("2006-01-02", "1986-03-07")
-
-	highlander := models.Movie{
-		ID:  1,
-		Title : "Highlander",
-		ReleaseDate: rd,
-		MPAARating: "R",
-		RunTime: 116,
-		Description: "A very nice movie",
-		CreatedAt: time.Now(),
-		UpdatedAt: time.Now(),
+	movies, err := app.DB.AllMovies()
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
-
-	movies = append(movies, highlander)
-
-	rd, _ = time.Parse("2006-01-02", "1981-06-12")
-
-	rota := models.Movie{
-		ID:  2,
-		Title : "Raiders of the Lost Ark",
-		ReleaseDate: rd,
-		MPAARating: "PG-13",
-		RunTime: 115,
-		Description: "Another very nice movie",
-		CreatedAt: time.Now(),
-		UpdatedAt: time.Now(),
-	}
-
-	movies = append(movies, rota)
 
 	out, err := json.Marshal(movies)
 

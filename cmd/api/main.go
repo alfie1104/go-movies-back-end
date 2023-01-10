469902a44010401cce938dbeaf581d2fa692e1b5
peter_alfie <alfie1104@gmail.com>
2023-01-10 13:38:34 +0900

    Installing a database driver and connecting to Postgres

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,31 +1,51 @@
 package main
 
 import (
+	"database/sql"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 const port = 8080
 
 type application struct {
+	DSN string
 	Domain string
+	DB *sql.DB
 }
 
 func main() {
 	// set application config
 	var app application
 
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	// read from command line
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=" + os.Getenv("DB_USER") + " password=" + os.Getenv("DB_PASSWORD") + " dbname=" + os.Getenv("DB_NAME") + " sslmode=disable timezone=UTC connect_timeout=5","Postgres connection string")
+	flag.Parse()
 
 	// connect to the database
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app.DB = conn
 
 	app.Domain = "example.com"
 
 	log.Println("Starting application on port", port)
 
 	// start a web server
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 
 	if err != nil {
 		log.Fatal(err)

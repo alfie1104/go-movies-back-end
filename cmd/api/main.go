17825d868a017b59baeebb6edb57edaf585278f6
peter_alfie <alfie1104@gmail.com>
2023-01-13 08:43:12 +0900

    Setting default values for JWT tokens

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -18,6 +19,11 @@ type application struct {
 	DSN string
 	Domain string
 	DB repository.DatabaseRepo
+	auth Auth
+	JWTSecret string
+	JWTIssuer string
+	JWTAudience string
+	CookieDomain string
 }
 
 func main() {
@@ -31,6 +37,11 @@ func main() {
 
 	// read from command line
 	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=" + os.Getenv("DB_USER") + " password=" + os.Getenv("DB_PASSWORD") + " dbname=" + os.Getenv("DB_NAME") + " sslmode=disable timezone=UTC connect_timeout=5","Postgres connection string")
+	flag.StringVar(&app.JWTSecret, "jwt-secret","verysecret","signing secret")
+	flag.StringVar(&app.JWTIssuer, "jwt-issuer","example.com","signing issuer")
+	flag.StringVar(&app.JWTAudience, "jwt-audience","example.com","signing audience")
+	flag.StringVar(&app.CookieDomain, "cookie-domain","localhost","cookie domain")
+	flag.StringVar(&app.Domain, "domain","example.com","domain")
 	flag.Parse()
 
 	// connect to the database
@@ -42,7 +53,16 @@ func main() {
 	app.DB = &dbrepo.PostgresDBRepo{DB: conn}
 	defer app.DB.Connection().Close()
 
-	app.Domain = "example.com"
+	app.auth = Auth{
+		Issuer: app.JWTIssuer,
+		Audience: app.JWTAudience,
+		Secret: app.JWTSecret,
+		TokenExpiry: time.Minute * 15,
+		RefreshExpiry: time.Hour * 24,
+		CookiePath: "/",
+		CookieName: "__Host-refresh_token",
+		CookieDomain: app.CookieDomain,
+	}
 
 	log.Println("Starting application on port", port)
 

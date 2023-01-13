631361fbf653eadec8103841fee9a1440ea85166
peter_alfie <alfie1104@gmail.com>
2023-01-13 09:03:34 +0900

    Creating a handler to generate a JWT

--- cmd/api/handler.go
+++ cmd/api/handler.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
@@ -26,4 +27,30 @@ func (app *application) AllMovies(w http.ResponseWriter, r *http.Request){
 	}
 
 	_ = app.writeJSON(w, http.StatusOK, movies)
+}
+
+func (app *application) authenticate(w http.ResponseWriter, r *http.Request) {
+	// read json payload
+
+
+	// validate user against database
+
+	// check password
+
+	// create a jwt user
+	u := jwtUser{
+		ID:  1,
+		FirstName: "Admin",
+		LastName: "User",
+	}
+
+	// generate tokens
+	tokens, err := app.auth.GenerateTokenPair(&u)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	log.Println(tokens.Token)
+	w.Write([]byte(tokens.Token))
 }
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -16,6 +16,8 @@ func (app *application) routes() http.Handler {
 
 	mux.Get("/", app.Home)
 
+	mux.Get("/authenticate", app.authenticate)
+
 	mux.Get("/movies", app.AllMovies)
 
 	return mux

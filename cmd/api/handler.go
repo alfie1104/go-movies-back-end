8899bfaa1c5e26891959ae021f3d691066c593ce
peter_alfie <alfie1104@gmail.com>
2023-01-20 08:22:57 +0900

    add movie catalogue api

--- cmd/api/handler.go
+++ cmd/api/handler.go
@@ -147,5 +147,11 @@ func (app *application) logout(w http.ResponseWriter, r *http.Request){
 }
 
 func (app *application) MovieCatalog(w http.ResponseWriter, r *http.Request){
-	
+	movies, err := app.DB.AllMovies()
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	_ = app.writeJSON(w, http.StatusOK, movies)
 }

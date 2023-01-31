60314e9a7af017fef7f749bb9bbe68e7a53eff20
peter_alfie <alfie1104@gmail.com>
2023-02-01 08:47:58 +0900

    Create a handler for GraphQL

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -26,6 +26,8 @@ func (app *application) routes() http.Handler {
 	mux.Get("/genres", app.AllGenres)
 	mux.Get("/movies/genres/{id}", app.AllMoviesByGenre)
 
+	mux.Get("/graph", app.moviesGraphQL)
+
 
 	mux.Route("/admin", func(mux chi.Router){
 		mux.Use(app.authRequired)

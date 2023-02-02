149b2d5bbd18b8129406aae786302d598d6faac2
peter_alfie <alfie1104@gmail.com>
2023-02-03 08:14:28 +0900

    Build backend server file and dump db

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -4,7 +4,8 @@ import "net/http"
 
 func (app *application) enableCORS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-		w.Header().Set("Access-Control-Allow-Origin","http://localhost:3000")
+		//w.Header().Set("Access-Control-Allow-Origin","http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Origin","http://192.18.136.71")
 		w.Header().Set("Access-Control-Allow-Credentials","true")
 		
 		if r.Method == "OPTIONS" {
--- command.md
+++ command.md
@@ -1,8 +1,7 @@
 # Go build
 
-- 명령어(for Mac) : CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o gomovies ./cmd/api
+- 명령어 : CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o gomovies ./cmd/api
 
 # Postgress DB dump
 
-- pg_dump를 설치
 - 명령어 : pg_dump --no-owner -h DB주소(예: localhost) -p DB포트(예: 5432) -u 사용자명(예:user) DB명(예: movies) > 출력파일명(예: movies.sql)
